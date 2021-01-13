357dd4595b0d8a98926e7f3368be1be0a3368043
Julianne Vela <cjvela@outlook.com>
2021-01-12 16:14:13 -0800

    added check for remaining guesses to disable button

--- app.js
+++ app.js
@@ -10,19 +10,26 @@ const remainingGuessesSpan = document.getElementById('remaining-guesses');
 // initialize state
 let remGuesses = 4;
 let correctNumber = Math.ceil(Math.random() * 20);
-
+console.log(correctNumber);
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
-    --remGuesses;
-    const guessNum = Number(userGuess.value);
-    if (compareNumbers(guessNum.value, correctNumber) === 0) {
-        resultsStr.textContent = 'YOU GOT IT! CONGRATS!';
-    } else if (compareNumbers(guessNum.value, correctNumber) === -1) {
-        resultsStr.textContent = 'Whoops, a little too low. Guess again!';
-    } else if (compareNumbers(guessNum.value, correctNumber) === 1) {
-        resultsStr.textContent = 'Darn, too high! Guess again!';
+    if (remGuesses < 1) {
+        submitButton.disabled = true;
     }
 
-    remainingGuessesSpan.textContent = remGuesses;
+    --remGuesses;
+    const guessNum = Number(userGuess.value);
+
+
+
+    if (compareNumbers(guessNum, correctNumber) === 0) {
+        resultsStr.textContent = 'YOU GOT IT! CONGRATS!';
+    } else if (compareNumbers(guessNum.value, correctNumber) === -1) {
+        userHintStr.textContent = `Whoops, a little too low. Guess again! ${remGuesses} left!`;
+    } else if (compareNumbers(guessNum.value, correctNumber) === 1) {
+        userHintStr.textContent = `Darn, too high! Guess again! ${remGuesses} left!`;
+    }
+
+    // remainingGuessesSpan.textContent = remGuesses;
 
 });
